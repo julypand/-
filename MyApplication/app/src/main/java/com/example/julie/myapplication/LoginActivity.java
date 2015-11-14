4155aa7c1d615f05f1647749324fc0082612740c
Natalie <fecit@inbox.ru>
2015-11-14 16:14:38 +0300

    привет, Юля

--- MyApplication/app/src/main/java/com/example/julie/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/julie/myapplication/LoginActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+//привет, Юлечка
 public class LoginActivity extends AppCompatActivity {
 
     Button loginBtn, singupBtn;
