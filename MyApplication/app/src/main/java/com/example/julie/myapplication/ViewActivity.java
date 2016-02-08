e4fd51d664a9a9a229e9ea07aec13743cd8fb07e
Natalie Pakki <fecitinboxru@gmail.com>
2016-02-09 00:45:05 +0300

    Change ViewActivity.java

--- MyApplication/app/src/main/java/com/example/julie/myapplication/ViewActivity.java
+++ MyApplication/app/src/main/java/com/example/julie/myapplication/ViewActivity.java
@@ -6,12 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import java.util.ArrayList;
 
 
 public class ViewActivity extends AppCompatActivity {
 
-    public ArrayList<Button> week = new ArrayList<>();
+    Button btnMonday, btnTuesday, btnWednesday, btnThursday, btnFriday, btnSaturday;
     int group = 0;
 
 
@@ -20,26 +19,59 @@ public class ViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
 
-        week.add((Button) findViewById(R.id.btnMonday));
-        week.add((Button) findViewById(R.id.btnTuesday));
-        week.add((Button) findViewById(R.id.btnWednesday));
-        week.add((Button) findViewById(R.id.btnThursday));
-        week.add((Button) findViewById(R.id.btnFriday));
-        week.add((Button) findViewById(R.id.btnSaturday));
         group = getIntent().getIntExtra("group", 0);
 
-        for (final Button btn : week) {
+        btnMonday = (Button) findViewById(R.id.btnMonday);
+        btnTuesday = (Button) findViewById(R.id.btnTuesday);
+        btnWednesday = (Button) findViewById(R.id.btnWednesday);
+        btnThursday = (Button) findViewById(R.id.btnThursday);
+        btnFriday = (Button) findViewById(R.id.btnFriday);
+        btnSaturday = (Button) findViewById(R.id.btnSaturday);
 
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    final Intent intent = new Intent(ViewActivity.this, ListClassesActivity.class);
-                    intent.putExtra("day", btn.getText().toString());
-                    intent.putExtra("group", group);
-                    startActivity(intent);
-                }
-            });
-        }
+
+        btnMonday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  goDay("Monday");
+            }
+        });
+        btnTuesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goDay("Tuesday");
+            }
+        });
+        btnWednesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goDay("Wednesday");
+            }
+        });
+        btnThursday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goDay("Thursday");
+            }
+        });
+        btnFriday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goDay("Friday");
+            }
+        });
+        btnSaturday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goDay("Saturday");
+            }
+        });
+
+    }
+    public void goDay(String day){
+        final Intent intent = new Intent(ViewActivity.this, ListClassesActivity.class);
+        intent.putExtra("day", day);
+        intent.putExtra("group", group);
+        startActivity(intent);
     }
 
 
